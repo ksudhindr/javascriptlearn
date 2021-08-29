431ded4bba98f8638c70f93e4fa489c272be3679
Sudhindra <sudhindra.kembhavi@audinate.com>
2021-08-29 15:48:12 +1000

    Playwright fixes

--- externmodules.js
+++ externmodules.js
@@ -1,7 +1,25 @@
-var fs = require('fs');
+//var fs = require('fs');
 var http = require('http');
 const playwright = require('playwright');
 console.log(typeof(playwright));
+/* var chromeBrowser = playwright.chromium.launch();
+var firefoxBroswer = playwright.firefox.launch();
+
+const context = chromeBrowser.newContext();
+const page =  context.newPage();
+ page.goto("https://playwright.dev");
+ page.waitForTimeout(5000);
+ browser.close(); */
+
+
+(async function(){
+  const browser = await playwright.webkit.launch({ headless: false }); // Non-headless mode to feel comfy
+  const context = await browser.newContext(); // So much to say, but another time
+  const page = await context.newPage(); // Create a new Page instance which handles most of your needs
+  await page.goto("https://playwright.dev"); // Navigate to the Playwright webpage
+  await page.waitForTimeout(5000); // Rest your eyes for five seconds
+  await browser.close(); // Close the browser
+})();
 
 /* var browser = await playwright.chromium.launch();
 var context = await browser.context();
